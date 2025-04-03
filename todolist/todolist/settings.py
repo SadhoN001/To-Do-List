21af432475a39f4576afd30aca6d6c6827ebd0b6
Sadhon Kumar Dev <skdsadhon@gmail.com>
2025-04-03 19:53:25 +0600

    update2

--- todolist/todolist/settings.py
+++ todolist/todolist/settings.py
@@ -12,6 +12,9 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 from pathlib import Path
 import os
+import environ
+env= environ.Env()
+environ.Env.read_env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-&1t@h7l(c(rrynf=8*xafe)esq&3fdq8m@*@1z%+y$c1+$t$=v'
+SECRET_KEY = env("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
